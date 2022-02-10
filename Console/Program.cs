a6654931be387bbb6f1809d841d6c38966bde9cb
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-10 19:48:58 +0200

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
