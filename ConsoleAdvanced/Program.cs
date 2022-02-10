3a14c4acfe9992cd29c40614aca8eafb4993d6ce
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-10 20:06:09 +0200

    Implemented Max of two numbers

--- ConsoleAdvanced/Program.cs
+++ ConsoleAdvanced/Program.cs
@@ -4,6 +4,11 @@ namespace ConsoleAdvanced
 {
     class Program
     {
+        public static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
